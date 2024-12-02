4aa95ed5ac50699015e118acd7e4cb62bcc6a2a2
Nicolas Verachtert <nicolas.verachtert@student.kdg.be>
2024-12-02 21:04:06 +0000

    feat: add chatbot documentation

--- config/environment.py
+++ config/environment.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 import os
-import base64
 
 # Load environment variables from a .env file
 load_dotenv()
@@ -10,9 +9,6 @@ GOOGLE_SERVICE_ACCOUNT_CRED_B64 = os.getenv("GOOGLE_SERVICE_ACCOUNT_CRED_B64")
 
 # Application configuration
 DATA_PATH = os.getenv("DATA_FOLDER", "data")
-GEMINI_EMBED_MODEL = os.getenv("GEMINI_EMBEDDING_MODEL", "models/embedding-001")
-GEMINI_LLM_MODEL = os.getenv("GEMINI_LLM_MODEL", "gemini-1.5-flash-latest")
-MISTRAL_LLM_MODEL = os.getenv("MISTRAL_LLM_MODEL", "open-mistral-7b")
 
 # Server configuration
 UVICORN_PORT = int(os.getenv("UVICORN_PORT", 5000))
@@ -25,9 +21,15 @@ CHROMA_RESET = bool(os.getenv("CRHOMA_RESET", "false"))
 
 # Google Cloud configuration
 GOOGLE_PROJECT_ID = os.getenv("GOOGLE_PROJECT_ID", "integratieproject-2-442110")
-GOOGLE_GEMINI_KEY_ID = os.getenv("GOOGLE_GEMINI_KEY_ID", "GEMINI_API_KEY")
+
 GCS_BUCKET_NAME=os.getenv("GCS_BUCKET_NAME", "team-20-monopoly-ip2-media-bucket")
 GCS_DOCS_PATH = os.getenv("GCS_DOCS_PATH", "monopoly-user-guides")
 
+# Gemini AI configuration
+GEMINI_EMBED_MODEL = os.getenv("GEMINI_EMBEDDING_MODEL", "models/embedding-001")
+GEMINI_LLM_MODEL = os.getenv("GEMINI_LLM_MODEL", "gemini-1.5-flash-latest")
+GEMINI_KEY_ID = os.getenv("GOOGLE_GEMINI_KEY_ID", "GEMINI_API_KEY")
+
 # Mistral AI configuration
 MISTRAL_KEY_ID = os.getenv("MISTRAL_KEY_ID", "MISTRAL_API_KEY")
+MISTRAL_LLM_MODEL = os.getenv("MISTRAL_LLM_MODEL", "open-mistral-7b")
